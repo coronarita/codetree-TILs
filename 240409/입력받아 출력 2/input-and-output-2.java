4d9721e40dfad947c9538e9c9383d7e117d52e22
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-09 15:26:16 +0900

    Title: 입력받아 출력 2, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240409/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+// import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String[] strArr = sc.next().split("-");
+        System.out.print(strArr[0]+strArr[1]);
+    }
+}
