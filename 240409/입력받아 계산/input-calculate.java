69dc7bc1769f4d167f78b379d49d076f0ecedae5
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-09 14:25:32 +0900

    Title: 입력받아 계산, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240409/입력받아 계산/input-calculate.java
+++ 240409/입력받아 계산/input-calculate.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        a = sc.nextInt();
+        int a = sc.nextInt();
         System.out.print(a+2);
     }
 }
