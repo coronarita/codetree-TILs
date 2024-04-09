cb3762a3d2514d914fbb8cb67665a4fd9fe6e54a
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-09 15:14:59 +0900

    Title: 1시간 뒤 시간 출력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240409/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 1시간 뒤의 시간을 출력
+        String[] timeArr = sc.next().split(":");
+
+        System.out.printf("%d:%d", timeArr[0]+1, timeArr[1]);
+                
+
+    }
+}
