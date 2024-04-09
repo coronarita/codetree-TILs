dcb687e5077d36aaea63f0da2d61a4e198d49956
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-09 15:34:00 +0900

    Title: 날짜 변경하여 출력, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240409/날짜 변경하여 출력/print-date-with-different-format.java
+++ 240409/날짜 변경하여 출력/print-date-with-different-format.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        String[] strArr = sc.next().split(".");
+        String[] strArr = sc.next().split("\\.");
         System.out.printf(strArr[2]+'.'+strArr[1]+'.'+strArr[0]);
     }
 }
