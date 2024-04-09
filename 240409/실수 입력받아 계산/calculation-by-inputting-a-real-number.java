d01eac6476bd484003478f4f5b69b104156e5700
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-09 14:51:57 +0900

    Title: 실수 입력받아 계산, Time: 173ms, Memory: 12MB, Status: Passed - CodeTree

--- 240409/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
+++ 240409/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -4,8 +4,8 @@ public class Main{
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
 
-        double a = nextDouble();
-        double b = nextDouble();
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
 
         System.out.printf("%.2f", a + b);
     }
