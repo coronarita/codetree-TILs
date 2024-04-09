0a4c72e9544bd54f5238b5c29e8eadf7aeaaae3f
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-09 13:42:30 +0900

    Title: 달에서 무게 구하기, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

--- 240409/달에서 무게 구하기/weight-on-the-moon.java
+++ 240409/달에서 무게 구하기/weight-on-the-moon.java
@@ -3,6 +3,6 @@ public class Main {
         int m = 13;
         double g = 0.165000; 
 
-        System.out.printf("%d * %.6f =  %.6f", m, g, m*g);
+        System.out.printf("%d * %.6f = %.6f", m, g, m*g);
     }
 }
