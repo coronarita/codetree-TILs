ccc83a70f6ce36fa05d6525b8cc26af52a6fe8af
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-09 15:27:21 +0900

    Title: 날짜 변경하여 출력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240409/날짜 변경하여 출력/print-date-with-different-format.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String[] strArr = sc.next().split(".");
+        System.out.printf("%d.%d.%d",strArr[2],strArr[1],strArr[2]);
+    }
+}
