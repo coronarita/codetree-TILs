30184e6078da076b21be71fbde97972df5b4611c
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-09 16:34:41 +0900

    Title: 살 수 있는 물건, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240409/살 수 있는 물건/things-able-to-buy.java
+++ 240409/살 수 있는 물건/things-able-to-buy.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        n = sc.nextInt();
+        int n = sc.nextInt();
         String s = "";
         // 살 수 있는 가장 비싼 물건
         if (n>=3000) s = "book";
