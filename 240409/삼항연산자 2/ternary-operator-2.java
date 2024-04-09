43c802800dfc09b5b38e56da0f32a69c6d4af2b2
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-09 16:31:49 +0900

    Title: 삼항연산자 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240409/삼항연산자 2/ternary-operator-2.java
+++ 240409/삼항연산자 2/ternary-operator-2.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        a = nextInt();
+        int a = nextInt();
         
         System.out.print(a==1?'t':'f');
     }
