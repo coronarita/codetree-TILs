3723b06eda9f61638cbcb6b2c0e129ab1cd9d9f9
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-09 14:27:18 +0900

    Title: 점수 입출력, Time: 112ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240409/점수 입출력/enter-int-and-print-score.java
+++ 240409/점수 입출력/enter-int-and-print-score.java
@@ -6,6 +6,6 @@ public class Main {
 
         int n = sc.nextInt();
 
-        System.out.println("Your score is %d point.", n);
+        System.out.println("Your score is" + n +  "point.");
     }
 }
