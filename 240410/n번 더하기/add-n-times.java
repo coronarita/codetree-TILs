926053c84267e4bba73051a587a941fadc89fe00
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-10 13:22:27 +0900

    Title: n번 더하기, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240410/n번 더하기/add-n-times.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+
+        // a에 n을 더하는 과정을 n번 반복
+
+        int cnt = 0;
+        while (cnt <= n ){
+            a += n;
+            System.out.println(a);
+            cnt++;
+        }
+        
+    }
+}
