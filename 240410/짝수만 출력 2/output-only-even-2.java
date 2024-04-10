8dfba6826d4c9f2a74bbb0874769133733d340f8
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-10 13:19:46 +0900

    Title: 짝수만 출력 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240410/짝수만 출력 2/output-only-even-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(system.in);
+
+        int b = sc.nextInt();
+        int a = sc.nextInt();
+
+        do {
+            if (b%2 == 0)
+            System.out.print(b + " ");
+        } while ( b >= a);
+    }
+}
