f80503d3c2c7b7d8182fc004285aa333c0f5bc5c
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-10 14:09:43 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240410/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.java
+++ 240410/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.java
@@ -8,8 +8,7 @@ public class Main {
 
         for (int i=0;i<n;i++){
             for (int j=0;j<n; j++){
-                System.out.print(n-j);
-                System.out.print(' ');
+                System.out.print((n-j) + " ");
             }
             System.out.println();
         }
