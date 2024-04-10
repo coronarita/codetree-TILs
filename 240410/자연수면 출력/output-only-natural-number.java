c25b3d9a9f167c33aeefc72d9b6aaa472832472a
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-10 13:28:42 +0900

    Title: 자연수면 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240410/자연수면 출력/output-only-natural-number.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int cnt = 0;
+        
+        if (a>0){
+            while (cnt <= n ){
+            System.out.print(a);
+            cnt++;
+            }
+        } else
+            System.out.println(0);
+        
+        
+    }
+}
