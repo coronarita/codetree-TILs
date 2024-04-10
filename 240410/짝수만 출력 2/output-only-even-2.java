d55e3c7c4ba35be1a290f575b73fe5493e0fda53
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-10 13:20:25 +0900

    Title: 짝수만 출력 2, Time: 3105ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

--- 240410/짝수만 출력 2/output-only-even-2.java
+++ 240410/짝수만 출력 2/output-only-even-2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        Scanner sc = new Scanner(system.in);
+        Scanner sc = new Scanner(System.in);
 
         int b = sc.nextInt();
         int a = sc.nextInt();
