f124a22b6abf76b620c9cbbee6b027272a0c0d89
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-04-10 16:24:53 +0900

    Title: 특정 원소들의 합, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 240410/특정 원소들의 합/sum-of-specific-elements.java
+++ 240410/특정 원소들의 합/sum-of-specific-elements.java
@@ -9,7 +9,7 @@ public class Main {
         int sum =0;
         
         for (int i=0; i<4;i++){
-            for (int j=0; j<=4;j++){
+            for (int j=0; j<4;j++){
                 arr[i][j] = sc.nextInt();
             }
         }
