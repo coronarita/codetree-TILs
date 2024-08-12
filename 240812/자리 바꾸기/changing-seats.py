858cc3d5073391cfcfc7bad728d108d67c2c2d67
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-08-12 15:00:43 +0900

    Title: 자리 바꾸기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240812/자리 바꾸기/changing-seats.py
+++ 240812/자리 바꾸기/changing-seats.py
@@ -1,34 +1,44 @@
+# 자리 바꾸기 2024-08-12 13:47
+
+# n개의 줄에 거쳐 I번째 줄에 I번 사람이 자리바꿈이 이뤄지는 동안 앉게되는 자리의 개수
+
 N, K = map(int, input().split())
-rule = [tuple(map(lambda x: int(x) - 1, input().split())) for _ in range(K)]
+o_list = [i for i in range(N)]
+p_list = [i for i in range(N)]
+# 각자 몇 군데의 자리에 앉을 수 있는지를 구하는 프로그램 - 무한반복 시 초기상태로 돌아오게 됨 !
 
-# 비트마스킹을 위한 초기화
-visited = [1 << i for i in range(N)]
-position = list(range(N))
+# 방문여부를 처리하기 위해서, n x n의 행렬을 정의
+# visited = [[False for _ in range(N)] for _ in range(N)]
+visited = [set() for _ in range(N)] # 중복처리
 
-# 주기 계산
-def calculate_cycle():
+rule = [tuple(map(int, input().split())) for _ in range(K)]
+idx = 0
+# 초기방문처리
+for i in range(N):
+    # visited[i][i] = True
+    visited[i].add(i)
+
+while True:
+
+    # swap rule
+    r_idx = idx % K
+    a, b = rule[r_idx]
     
-    cycle = 0
-    while True:
-        cycle += 1
-        for a, b in rule:
-            position[a], position[b] = position[b], position[a]
-            visited[position[a]] |= 1 << a
-            visited[position[b]] |= 1 << b
-        if position == list(range(N)):
-            return cycle
+    # 몇번 사람 ? - a-1에 있는 사람
+    a_num, b_num = p_list[a-1], p_list[b-1]
+    p_list[a-1], p_list[b-1] = p_list[b-1], p_list[a-1]
+    
+    # 몇번 사람 - 어디에 갔는지
+    # visited[a_num][b-1] = True
+    # visited[b_num][a-1] = True
+    # print(p_list)
+    visited[a_num].add(b-1)
+    visited[b_num].add(a-1)
+    
+    idx += 1
+    if o_list == p_list or idx == 3*K : 
+        break
+    
 
-# 주기 계산
-cycle = calculate_cycle()
-
-# 남은 반복 처리
-remaining = (N * K) % cycle
-for _ in range(remaining):
-    for a, b in rule:
-        position[a], position[b] = position[b], position[a]
-        visited[position[a]] |= 1 << a
-        visited[position[b]] |= 1 << b
-
-# 결과 출력
-for v in visited:
-    print(bin(v).count('1'))
+for i in range(N):
+    print(len(visited[i]))
