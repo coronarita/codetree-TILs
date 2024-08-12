5f986205a20995a8bc0f4c73b891d81394c8a78e
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-08-12 14:35:54 +0900

    Title: 자리 바꾸기, Time: 5065ms, Memory: 147MB, Status: Time Limit Exceed - CodeTree

--- 240812/자리 바꾸기/changing-seats.py
+++ 240812/자리 바꾸기/changing-seats.py
@@ -6,14 +6,17 @@ N, K = map(int, input().split())
 o_list = [i for i in range(N)]
 p_list = [i for i in range(N)]
 # 각자 몇 군데의 자리에 앉을 수 있는지를 구하는 프로그램 - 무한반복 시 초기상태로 돌아오게 됨 !
-# 방문여부를 처리하기 위해서, n x n의 행렬을 정의
-visited = [[False for _ in range(N)] for _ in range(N)]
-rule = [tuple(map(int, input().split())) for _ in range(K)] # i분에는 prv, nxt번째 자리의 사람이 서로 바꿈
 
+# 방문여부를 처리하기 위해서, n x n의 행렬을 정의
+# visited = [[False for _ in range(N)] for _ in range(N)]
+visited = [set() for _ in range(N)] # 중복처리
+
+rule = [tuple(map(int, input().split())) for _ in range(K)]
 idx = 0
 # 초기방문처리
 for i in range(N):
-    visited[i][i] = True
+    # visited[i][i] = True
+    visited[i].add(i)
 
 while True:
 
@@ -26,13 +29,16 @@ while True:
     p_list[a-1], p_list[b-1] = p_list[b-1], p_list[a-1]
     
     # 몇번 사람 - 어디에 갔는지
-    visited[a_num][b-1] = True
-    visited[b_num][a-1] = True
+    # visited[a_num][b-1] = True
+    # visited[b_num][a-1] = True
     # print(p_list)
+    visited[a_num].add(b-1)
+    visited[b_num].add(a-1)
+    
 
     if o_list == p_list : 
         break
     idx += 1
 
 for i in range(N):
-    print(sum(visited[i]))
+    print(len(visited[i]))
