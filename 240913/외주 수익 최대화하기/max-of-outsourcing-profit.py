74f42a68ea1c22e72eaa51b3bf7138be8a3670d2
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2024-09-13 22:04:09 +0900

    Title: 외주 수익 최대화하기, Time: 160ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240913/외주 수익 최대화하기/max-of-outsourcing-profit.py
+++ 240913/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -1,4 +1,28 @@
 n = int(input())
+works = []
 
-works = [tuple(map(int, input().split())) for _ in range(n)]
-print(works)
+# 외주수익 최대화 (t, p), (idx+1)일차 
+# 2 5 / 2 7 / 1 3
+# 0 - 2 / 5
+# 1 - 3 / 7
+# 2 - 3 / 3
+
+for s in range(n):
+    t, p = map(int, input().split())
+    e = s + t
+    works.append((s, e, p, t))
+
+works.sort(key=lambda x:(-x[2], x[3], x[0]))
+ans = 0
+
+plans = [0] * n
+for work in works : 
+    s, e, p, t = work
+    # 일이 가능한지 판단
+    if not all(plans[s:e]) :
+        for i in range(s, e):
+            plans[i] = 1
+        ans += p
+
+# print(plans)
+print(ans)
